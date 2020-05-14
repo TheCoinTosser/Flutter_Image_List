57e0ac07ae9a4d582c24ddf5f84007959a2d0a1a
Tiago Catelam <tiago.catelam@tigerspike.com>
2020-05-14 16:26:36 +1000

    Added time dilation debug code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutterpresentation/screens/list/PhotoListScaffoldAndroid.dart';
 import 'package:flutterpresentation/screens/list/PhotoListScaffoldCupertino.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
 
 void main() => runApp(MyApp());
 
@@ -10,6 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    timeDilation = 1.0;
+
     switch(Theme.of(context).platform){
       case TargetPlatform.iOS: return PhotoListScaffoldCupertino(barTitle: title);
       default: return PhotoListScaffoldAndroid(barTitle: title);
